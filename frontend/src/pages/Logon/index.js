9a9cf8c415fbf11fd9a90c960424a46b72854c60
BavarescoM <mauricio.bavaresco@unochapeco.edu.br>
2020-03-26 19:59:54 -0300

    continuar 39:18

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import './styles.css';
+import 	heroesImg from '../../assets/heroes.png';
+import 	logoImg from '../../assets/logo.svg';
+import { FiLogIn} from 'react-icons/fi';
+
+export default function Logon() {
+	return (
+		<div className="logon-container">
+		<section className="form">
+		<img src={logoImg} alt="Be the Hero" />
+		<form>
+		<h1>Faça seu Logon</h1>
+		<input type="text" placeholder="Sua ID"/>
+		<button type="submit">Entrar</button>
+		<a href="/register">
+			<FiLogIn size={16} color="#e02041"/>
+		Não tem cadastro.</a>
+		</form>
+
+
+		</section>
+		<img src={heroesImg} alt="Heroes" />
+		</div>
+	);
+}
